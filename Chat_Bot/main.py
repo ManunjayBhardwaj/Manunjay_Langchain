998bd29c85b29ae0a12e6219f203c6cce4226d9c
Manunjay Bhardwaj <156889839+ManunjayBhardwaj@users.noreply.github.com>
2025-06-19 16:48:25 +0530

    Add files via upload

--- Chat_Bot/main.py
+++ Chat_Bot/main.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 load_dotenv() # to load the .env file
 
 # environment variables call
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = "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"
 
 # Langsmith tracking
-os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_API_KEY'] = "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"
 os.environ['LANGCHAIN_TRACING_V2']="true"
 
 
 # Creating Chatbot
 prompt=ChatPromptTemplate.from_messages([
-    ("system","You are a helpfull assistant,Please provide response to the user queries"),
+    ("system","You are a help full assistant,Please provide response to the user queries"),
     ("user","Question:{question}")
     ]
 )
@@ -28,7 +28,7 @@ input_txt=st.text_input("Search the topic you are looking for")
 # open ai llm call
 llm = ChatOpenAI(
     model="llama3-70b-8192",
-    openai_api_key=os.getenv('OPENAI_API_KEY'),
+    openai_api_key="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",
     openai_api_base="https://api.groq.com/openai/v1",
     temperature=0.7
 )
